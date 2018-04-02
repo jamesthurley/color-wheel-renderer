6f375e2f6b3efebf5b05e8d6de0f55f5b7c43bef
James Thurley <mail@jamesthurley.com>
2018-04-02 19:20:57 +0100

    Add gifwrap for gif generation.

--- src/rendering/gif-snapshot-consumer.ts
+++ src/rendering/gif-snapshot-consumer.ts
@@ -1,8 +1,14 @@
 import { ISnapshotConsumer } from '../recording/snapshot-consumers/snapshot-consumer';
 import { Snapshot } from '../recording/snapshot';
+import { GifFrame, BitmapImage } from 'gifwrap';
 
 export class GifSnapshotConsumer implements ISnapshotConsumer {
+
+  private readonly frames: GifFrame[] = [];
+
   public consume(snapshot: Snapshot): Promise<void> {
-    throw new Error('Method not implemented.');
+    const frame = new GifFrame(new BitmapImage(snapshot.photo.bitmap));
+    this.frames.push(frame);
+    return Promise.resolve();
   }
 }
