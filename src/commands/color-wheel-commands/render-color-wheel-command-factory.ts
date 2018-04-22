831b50823fb62f50e1e9119506380032a7442d3c
James Thurley <mail@jamesthurley.com>
2018-04-22 18:10:07 +0100

    Remove redundant imports.

--- src/commands/color-wheel-commands/render-color-wheel-command-factory.ts
+++ src/commands/color-wheel-commands/render-color-wheel-command-factory.ts
@@ -1,9 +1,7 @@
 import { ICommandFactory } from '../command-factory';
 import { ICommand } from '../command';
 import { ColorWheelOptions } from './color-wheel-options';
-import { DisplayableError } from '../../common/displayable-error';
 import { RenderColorWheelCommand } from './render-color-wheel-command';
-import { Size } from '../../common/size';
 
 export class RenderColorWheelCommandFactory implements ICommandFactory<ColorWheelOptions> {
   public create(options: ColorWheelOptions): ICommand {
