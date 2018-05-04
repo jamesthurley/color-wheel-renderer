705524bb6cc97d743d438e4c6c46998182131b9d
James Thurley <mail@jamesthurley.com>
2018-05-04 19:06:01 +0100

    Verbose logging in integration tests.

--- integration-tests/recording.spec.ts
+++ integration-tests/recording.spec.ts
@@ -9,8 +9,11 @@ import { ComparingSnapshotConsumer } from '../src/sessions/testing/snapshot-cons
 import { IntegrationTestConsumerHelper } from '../src/sessions/testing/integration-test-consumer-helper';
 import { ISnapshotConsumer } from '../src/sessions/pipeline/snapshot-consumer';
 import { evaluateComparisons } from './evaluate-comparisons';
+import { Log } from '../src/common/log';
+import { LogLevel } from '../src/common/log-level';
 
 const macro: Macro = async (t, inputFolder: string, editorType: string) => {
+  Log.logLevel = LogLevel.verbose;
 
   const editorFactory = EditorFactoryMap.get(editorType);
   if (!editorFactory) {
--- integration-tests/rendering.spec.ts
+++ integration-tests/rendering.spec.ts
@@ -9,8 +9,11 @@ import { RenderingSnapshotConsumer } from '../src/sessions/rendering/snapshot-co
 import { ComparingFrameConsumer } from '../src/sessions/testing/frame-consumers/comparing-frame-consumer';
 import { evaluateComparisons } from './evaluate-comparisons';
 import { FrameFolderUtilities } from '../src/sessions/pipeline-common/frame-folder-utilities';
+import { Log } from '../src/common/log';
+import { LogLevel } from '../src/common/log-level';
 
 const macro: Macro = async (t, inputFolder: string) => {
+  Log.logLevel = LogLevel.verbose;
 
   inputFolder = './integration-tests/input-data/recorded-sessions/' + inputFolder;
 
